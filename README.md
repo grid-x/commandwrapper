d919a3f12f33150d5c1685687a8a6eaf253ec58f
ecktom <t.eck@gridx.de>
2018-11-23 09:46:54 +0100

    feat(init): Fix addressed problems

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # commandwrapper
 
+## Running multiple binaries in a scratch docker container
+If you only wanted to run a single executable, your life would be simple. CMD does not require a shell, so you could simply add:
+
+```
+FROM scratch
+...
+...
+CMD ["/file1"]
+```
+
+While running a single command is easy, the fact that you want to run two different commands in sequence means that you need something to handle that workflow. The easiest solution is a shell, because then you can simply run:
+
+```
+FROM scratch
+...
+...
+CMD /file1;/file2
+```
+
+As we do not want to include a shell in our scratch containers we're using this commandwrapper to call other binaries:
+
 ```
 /commandwrapper  \
 -execute="/usr/local/bin/envscan -server-address=device-api.gridx.de:80....." \
--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
 	"strings"
-	"sync"
 )
 
 type arrayFlags []string
@@ -25,7 +22,6 @@ func (i *arrayFlags) Set(value string) error {
 var myFlags arrayFlags
 
 func main() {
-	var wg sync.WaitGroup
 	flag.Var(&myFlags, "execute", "eg. -execute='/usr/local/bin/mybin test123 -o -a' -execute='ls -l'")
 	flag.Parse()
 
@@ -38,48 +34,12 @@ func main() {
 		s := strings.Split(element, " ")
 		cmdName, cmdArgs := s[0], s[1:]
 
-		var stdoutBuf, stderrBuf bytes.Buffer
 		cmd := exec.Command(cmdName, cmdArgs...)
+		cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
 
-		stdoutIn, err := cmd.StdoutPipe()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to create StdoutPipe: ", err)
-			os.Exit(1)
-		}
-		stderrIn, err := cmd.StderrPipe()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to create StderrPipe: ", err)
-			os.Exit(1)
-		}
-
-		var errStdout, errStderr error
-		stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
-		stderr := io.MultiWriter(os.Stderr, &stderrBuf)
-
-		if err := cmd.Start(); err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to start the command: ", err)
-			os.Exit(1)
-		}
-
-		wg.Add(2)
-		go func() {
-			_, errStdout = io.Copy(stdout, stdoutIn)
-			defer wg.Done()
-		}()
-
-		go func() {
-			_, errStderr = io.Copy(stderr, stderrIn)
-			defer wg.Done()
-		}()
-
-		if err := cmd.Wait(); err != nil {
+		if err := cmd.Run(); err != nil {
 			fmt.Fprintln(os.Stderr, "Command failed: ", err)
 			os.Exit(1)
 		}
-		if errStdout != nil || errStderr != nil {
-			fmt.Fprintln(os.Stderr, "Failed to capture stdout or stderr", err)
-			os.Exit(1)
-		}
 	}
-	wg.Wait()
 }
